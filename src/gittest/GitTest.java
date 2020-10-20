f8112882605926e28f25237ebdcf89af5fab1db3
Kantapon Tantrapornpong <kantapon.tantra@mail.kmutt.ac.th>
2020-10-20 15:12:21 +0700

    First Commit

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,11 @@
+package gittest;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+}
